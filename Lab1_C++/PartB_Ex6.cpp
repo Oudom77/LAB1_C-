e41d329fcac69474341b075b952cc43d0f512216
Oudom77 <gatorman4511@gmail.com>
2025-08-07 11:54:40 +0700

    Update and rename B_6.cpp to PartB_Ex6.cpp

--- Lab1_C++/PartB_Ex6.cpp
+++ Lab1_C++/PartB_Ex6.cpp
@@ -9,19 +9,19 @@ int main (){
     cout << "Enter a number: ";
     cin >> num;
 
-    bool isPrime = true;
+    bool isPrime = true;        //Holds the status of the number
 
-    for (int i = 2; i < num; i++){
+    for (int i = 2; i < num; i++){      //Loops from 2 -> number - 1
 
-        if (num % i == 0){
+        if (num % i == 0){              //Checks if it is divisible by a number other than 1 and itself
 
-            isPrime = false;
+            isPrime = false;            //If so change the status
             break;
 
         }
     }
 
-    if (isPrime){
+    if (isPrime){       //Print based on the status
 
         cout << "The number " << num << " is a prime number" << endl;
 
